d2478af132685a33d620c631745ca86878a7590d
ProfesseurOrme <matth.guilloteau@gmail.com>
2021-03-31 16:36:07 +0200

    Initialisation du projet ToDo&Co : migration application vers version LTS Symfony 4.4 complété

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class TaskController extends AbstractController
+{
+	/**
+	* @Route("/tasks", name="task_list")
+	*/
+	public function listTask()
+	{
+		return $this->render('task/list.html.twig', [
+			'tasks' => $this->getDoctrine()->getRepository('App:Task')->findAll()
+		]);
+	}
+
+	/**
+	* @Route("/tasks/create", name="task_create")
+	* @param Request $request
+	* @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+	*/
+	public function createTask(Request $request)
+	{
+		$task = new Task();
+		$form = $this->createForm(TaskType::class, $task);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$task->setCreatedAt(new \Datetime());
+			$task->setIsDone(false);
+			$em = $this->getDoctrine()->getManager();
+
+			$em->persist($task);
+			$em->flush();
+
+			$this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+			return $this->redirectToRoute('task_list');
+		}
+
+		return $this->render('task/create.html.twig', [
+			'form' => $form->createView()
+		]);
+    }
+
+	/**
+	* @Route("/tasks/{id}/edit", name="task_edit")
+	* @param Task $task
+	* @param Request $request
+	* @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+	*/
+	public function editTask(Task $task, Request $request)
+	{
+		$form = $this->createForm(TaskType::class, $task);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$this->getDoctrine()->getManager()->flush();
+
+			$this->addFlash('success', 'La tâche a bien été modifiée.');
+
+			return $this->redirectToRoute('task_list');
+		}
+
+		return $this->render('task/edit.html.twig', [
+			'form' => $form->createView(),
+			'task' => $task,
+		]);
+	}
+
+	/**
+	* @Route("/tasks/{id}/toggle", name="task_toggle")
+	* @param Task $task
+	* @return \Symfony\Component\HttpFoundation\RedirectResponse
+	*/
+	public function toggleTask(Task $task)
+	{
+		$task->setIsDone(!$task->getIsDone());
+		$this->getDoctrine()->getManager()->flush();
+
+		$this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+
+		return $this->redirectToRoute('task_list');
+	}
+
+	/**
+	* @Route("/tasks/{id}/delete", name="task_delete")
+	* @param Task $task
+	* @return \Symfony\Component\HttpFoundation\RedirectResponse
+	*/
+	public function deleteTask(Task $task)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$em->remove($task);
+		$em->flush();
+
+		$this->addFlash('success', 'La tâche a bien été supprimée.');
+
+		return $this->redirectToRoute('task_list');
+	}
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserController extends AbstractController
+{
+	/**
+	* @Route("/users", name="user_list")
+	*/
+	public function listUsers()
+	{
+		return $this->render('user/list.html.twig', [
+				'users' => $this->getDoctrine()->getRepository('App:User')->findAll()
+		]);
+	}
+
+	/**
+	* @Route("/users/create", name="user_create")
+	* @param Request $request
+	* @param UserPasswordEncoderInterface $passwordEncoder
+	* @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+	*/
+	public function createUser(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+	{
+		$user = new User();
+		$form = $this->createForm(UserType::class, $user);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$password = $passwordEncoder->encodePassword($user, $user->getPassword());
+			$user->setPassword($password);
+
+			$em->persist($user);
+			$em->flush();
+
+			$this->addFlash('success', "L'utilisateur a bien été ajouté.");
+
+			return $this->redirectToRoute('user_list');
+		}
+
+		return $this->render('user/create.html.twig', [
+			'form' => $form->createView()
+		]);
+	}
+
+	/**
+	* @Route("/users/{id}/edit", name="user_edit")
+	* @param User $user
+	* @param Request $request
+	* @param UserPasswordEncoderInterface $passwordEncoder
+	* @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+	*/
+	public function editUser(User $user, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+	{
+		$form = $this->createForm(UserType::class, $user);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+				$password = $passwordEncoder->encodePassword($user, $user->getPassword());
+				$user->setPassword($password);
+
+				$this->getDoctrine()->getManager()->flush();
+
+				$this->addFlash('success', "L'utilisateur a bien été modifié");
+
+				return $this->redirectToRoute('user_list');
+		}
+
+		return $this->render('user/edit.html.twig', [
+			'form' => $form->createView(),
+			'user' => $user
+		]);
+	}
+}
